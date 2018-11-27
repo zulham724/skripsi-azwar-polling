bdea51d1afae97dcc790b072be5d4d8940fbfc1f
zulham724 <zulham724@students.unnes.ac.id>
2018-11-27 20:32:08 +0700

    update

--- controllers/ActionController.js
+++ controllers/ActionController.js
@@ -1,6 +1,18 @@
 // import libraries
-var gpio = require('onoff').Gpio;
-var pin2 = new gpio(2,'out');
+var Gpio = require('pigpio').Gpio;
+var led = new Gpio(25,{mode: Gpio.OUTPUT});
+var in1 = new Gpio(23,{mode: Gpio.OUTPUT});
+var in2 = new Gpio(24,{mode: Gpio.OUTPUT});
+var enA = new Gpio(4,{mode:  Gpio.OUTPUT});
+var in3 = new Gpio(27,{mode: Gpio.OUTPUT});
+var in4 = new Gpio(22,{mode: Gpio.OUTPUT});
+var enB = new Gpio(17,{mode: Gpio.OUTPUT});
+in1.digitalWrite(1);
+in2.digitalWrite(0);
+enA.pwmWrite(0);
+in3.digitalWrite(0);
+in4.digitalWrite(1);
+enB.pwmWrite(0);
 // ============================
 var request = require('request');
 var sizeof = require('object-sizeof');
@@ -13,11 +25,13 @@ var current_action = null;
 var bandwidth = 0;
 var cpuUsage = 0;
 var memoryUsage = 0;
+var interval = 500;
 
 // setup module
 module.exports = {
 	get: function(req,res){
 		// do something
+		
 		console.log('please wait passport security on proccess');
 		request.get({
 			url:url+'api/clients/2',
@@ -38,6 +52,7 @@ module.exports = {
 				}
 			},function(err,resp,body){
 				console.log('token ready! \n\n',body.access_token,'\n\n');
+				console.log('now we will request data to server with interval ',interval,'ms\n');
 				token = body.access_token;
 				
 				setInterval(()=>{
@@ -62,20 +77,24 @@ module.exports = {
 							bandwidth += sizeof(body);
 							memoryUsage = os.totalmem() - os.freemem();
 							
-							if(current_action != (body.data != null ? body.data.name : null)){
+							if(current_action != body.message){
 							
-								if(body.data.name == 'forward'){
-									pin2.writeSync(1);
-								} else if(body.data.name == 'stop'){
-									pin2.writeSync(0);
+								if(body.message == 'forward'){
+									enA.pwmWrite(255);
+									enB.pwmWrite(255);
+									led.digitalWrite(1);
+								} else if(body.message == 'stop'){
+									enA.pwmWrite(0);
+									enB.pwmWrite(0);
+									led.digitalWrite(0);
 								}
 								
 								var date = new Date();
 								
-								current_action = body.data.name;
+								current_action = body.message;
 								
-								console.log('New data recieved at ',date.toString(),' with : ',current_action,'\n','estimate bandwidth used : ',bandwidth,' Bytes \n','estimate cpu usage : ',cpuUsage,'%\n','estimate memory used : ',memoryUsage,' Bytes\n');
-								console.log('data will sent to server again, please wait...');
+								console.log('New data recieved at ',date.toString(),' with : ',body,'\n','estimate bandwidth used : ',bandwidth,' Bytes \n','estimate cpu usage : ',cpuUsage,'%\n','estimate memory used : ',memoryUsage,' Bytes\n');
+								console.log('data will sent to server again, please wait...\n');
 								
 								request.post({
 									url:url+'api/pollings',
@@ -86,11 +105,14 @@ module.exports = {
 									},
 									body:{
 										user_id:1,
-										value:body.data.name,
-										date:date.toLocaleString()
+										value:body.message,
+										date:date.toLocaleString('en-US'),
+										bandwidth_usage:bandwidth,
+										cpu_usage:cpuUsage,
+										memory_usage:memoryUsage
 									}
 								},function(err,resp,body){
-									console.log('result :\n',body);
+									console.log('result :\n',body,'\n\n');
 								});
 								
 								
@@ -99,7 +121,7 @@ module.exports = {
 						}
 						
 					});
-				},500);
+				},interval);
 			});
 			
 		});
